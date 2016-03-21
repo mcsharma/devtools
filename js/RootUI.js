ba9d233a06e8378deabaf010b3aeecb3a729b576
MaheshSharma <mahesh.sharma@thoughtspot.com>
2016-03-20 20:36:34 -0700

    Grouped results from same file together

--- js/RootUI.js
+++ js/RootUI.js
@@ -2,9 +2,53 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var RootUI = React.createClass({
+    _getRowMarkup: function (leftMarkup, rightMarkup) {
+        if (typeof this.key == 'undefined') {
+            this.key = 0;
+        }
+        return <tr key={this.key++}>
+            <td className="resultLineNumber">{leftMarkup}</td>
+            <td className="resultFileView">{rightMarkup}</td>
+        </tr>
+    },
+
     render: function () {
         var i = 0;
-        var results = this.props.data['results'];
+        var rawResults = this.props.data['results'];
+        var results = {};
+        rawResults.forEach(function (result) {
+            var parts = result.substr(26).split(":");
+            if (!results[parts[0]]) results[parts[0]] = {};
+            results[parts[0]][parts[1]] = parts[2];
+        });
+        var markups = [];
+        for (var filePath in results) {
+            if (!results.hasOwnProperty(filePath)) continue;
+            var fileHref =
+                this.props.data.prefix +
+                filePath + "$" +
+                Object.keys(results[filePath]).join(","); // Phabricator will highlight these lines
+            var mainRow = this._getRowMarkup(
+                null,
+                <a href={fileHref}>{filePath}</a>
+            );
+            markups.push(mainRow);
+            for (var lineNum in results[filePath]) {
+                if (!results[filePath].hasOwnProperty(lineNum)) continue;
+                var line = results[filePath][lineNum];
+                var highlightedLine = this._highlightQuery(
+                    line,
+                    this.props.data['q']
+                );
+                var individualRow = this._getRowMarkup(
+                    lineNum,
+                    <div dangerouslySetInnerHTML={{__html: highlightedLine}}></div>
+                );
+                markups.push(individualRow);
+            }
+            // Add an empty line.
+            markups.push(this._getRowMarkup(null, <br />));
+        }
         return (
             <div>
                 <div>
@@ -15,32 +59,13 @@ var RootUI = React.createClass({
                             placeholder="Type text to search"
                         />
                         <input type="submit" value="Search"/>
-                        <span> {results.length} results found!</span>
+                        <span> {rawResults.length} results found!</span>
                     </form>
                 </div>
                 <div className="resultsTitle">Search Results:</div>
-                <div className="results code">
-                    {results.map(function (result) {
-                        var parts = result.substr(26).split(":");
-                        var url =
-                            this.props.data.prefix +
-                            parts[0] +
-                            "$" + parts[1];
-                        var text = parts[0] + ":" + parts[1];
-                        return (
-                            <div className="result" key={i++}>
-                                <div><a href={url}>{text}</a></div>
-                                <div dangerouslySetInnerHTML={{
-                                    __html: this._highlightQuery(
-                                        parts[2],
-                                        this.props.data['q'])
-                                    }}>
-                                </div>
-                                <br />
-                            </div>
-                        );
-                    }.bind(this))}
-                </div>
+                <table className="code" cellPadding="0" cellSpacing="0">
+                    <tbody>{markups}</tbody>
+                </table>
             </div>
         );
     },
