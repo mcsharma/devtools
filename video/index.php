dca74cdbf9bbaf3cec3b35984aaf8e2a4e998d42
MaheshSharma <mahesh.sharma@thoughtspot.com>
2016-03-28 11:38:41 -0700

    Added video/index.php

---
+++ video/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+<video width="100%" height="95%" controls>
+    <source src="codesearch.mov" type="video/mp4">
+    Your browser does not support the video tag.
+</video>
+</body>
+</html>
